c68fb5c99ba1d636f18700c0003e33618907765c
Pratap Chakravarthy <prataprc@gmail.com>
2013-04-15 18:48:25 +0530

    * curses dom-nodes are handled by normal process (previously they where   gen_server implementation). * Refactored `ncchan` module.

--- ncurses/src/ncchan.erl
+++ ncurses/src/ncchan.erl
@@ -3,7 +3,8 @@
 -behaviour(gen_server).
 
 % module API
--export([]).
+-export([start_link/1, new/1, subscribe/2, subscribe/3, subscribe/4,
+         unsubscribe/2, event/2, adddom/2, bubbleup/3]).
 
 % behaviour Callbacks
 -export([
@@ -15,19 +16,86 @@
 -include_lib("ncdom.hrl").
 
 %---- module API
+start_link(Args) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+new(Chname) ->
+    gen_server:call(?MODULE, {new, Chname}, infinity).
+
+subscribe(Chname, Proc) ->
+    gen_server:call(?MODULE, {subscribe, Chname, Proc}, infinity).
+
+subscribe(Chname, Mod, Fn) ->
+    gen_server:call(?MODULE, {subscribe, Chname, Mod, Fn, []}, infinity).
+
+subscribe(Chname, Mod, Fn, Args) ->
+    gen_server:call(?MODULE, {subscribe, Chname, Mod, Fn, Args}, infinity).
+
+unsubscribe(Chname, Ref) ->
+    gen_server:call(?MODULE, {unsubscribe, Chname, Ref}, infinity).
+
+event(Chname, Event) ->
+    gen_server:call(?MODULE, {event, Chname, Event}, infinity).
+
+adddom(AppPath, RootNode) ->
+    gen_server:call(?MODULE, {adddom, AppPath, RootNode}, infinity).
+
+bubbleup(AppPath, Name, Event) ->
+    gen_server:call(?MODULE, {bubbleup, AppPath, Name, Event}, infinity).
+
 
 %---- behaviour callbacks
 
-init({ParentPid, RootNode, Node}) ->
+init(_Args) ->
     process_flag(trap_exit, true),
-    {ok, {}}.
+    {ok, #evtsys{channels=[], appdoms=[]}}.
 
 
-handle_call(_Request, _From, State) ->
+handle_call({new, Chname}, _From, #evtsys{channels=Channels}=State) ->
+    {reply, ok,
+      State#evtsys{
+        channels=[#channel{name=Chname, subscribers=[]} | Channels] }};
+
+handle_call({subscribe, Chname, Mod, Fn, Args}, _From, State) ->
+    Ref = erlang:make_ref(),
+    case add_handler(Chname, {Ref, Mod, Fn, Args}, State) of
+        {false, NewState} -> {reply, false, NewState};
+        {true, NewState} -> {reply, Ref, NewState}
+    end;
+
+handle_call({subscribe, Chname, Proc}, _From, State) ->
+    Ref = erlang:make_ref(),
+    case add_handler(Chname, {Ref, Proc}, State) of
+        {false, NewState} -> {reply, false, NewState};
+        {true, NewState} -> {reply, Ref, NewState}
+    end;
+
+handle_call({unsubscribe, Chname, Ref}, _From, State) ->
+    {reply, ok, remove_handler(Chname, Ref, State)};
+
+handle_call({event, Chname, Event}, _From, State) ->
+    #evtsys{channels=Channels} = State,
+    #channel{subscribers=Subscrs} = lists:keyfind(Chname, 2, Channels),
+    doevent(Subscrs, Event),
+    {reply, ok, State};
+
+handle_call({adddom, AppPath, RootNode}, _From, State) ->
+    #evtsys{appdoms=Doms}=State,
+    {reply, ok, State#evtsys{appdoms=[{AppPath, RootNode} | Doms]}};
+
+handle_call({deldom, AppPath}, _From, State) ->
+    #evtsys{appdoms=Doms}=State,
+    {reply, ok, State#evtsys{appdoms=lists:keydelete(AppPath, 1, Doms)}};
+
+handle_call({bubbleup, AppPath, Name, Event}, _From, State) ->
+    #evtsys{appdoms=Doms}=State,
+    {AppPath, RootNode} = lists:keyfind(AppPath, 1, Doms),
+    bubble_event(Event, lists:reverse( nodepath( Name, RootNode ))),
     {reply, ok, State}.
 
 
-handle_info(_Info, State) ->
+handle_info({'EXIT', From, Reason}, State) ->
+    error_logger:info_msg("Process ~p exited because of ~p~n", [From, Reason]),
     {noreply, State}.
 
 
@@ -45,66 +113,52 @@ terminate(_Reason, _State) ->
 
 %---- internal functions
 
-new(Chname, Channels) ->
-    [{Chname, []} | Chans].
-
-subscribe(Chname, Mod, Fn, State) ->
-    subscribe(Chname, Mod, Fn, [], State).
-
-subscribe(Chname, Mod, Fn, Args, Channels) ->
-    #channel{subscribers=Subscrs}=Channel = lists:keyfind(Chname, 2, Channels),
-    Ref = erlang:make_ref(),
-    lists:keyreplace(
-        Chname, 2, Channels,
-        Channel#channel{subscribers=[{Ref, Mod, Fn, Args} | Subscrs]} ).
-
-subscribe(Chname, Proc, Channels) ->
-    #channel{subscribers=Subscrs}=Channel = lists:keyfind(Chname, 2, Channels),
-    Ref = erlang:make_ref(),
-    lists:keyreplace(
-        Chname, 2, Channels,
-        Channel#channel{subscribers=[{Ref, Proc} | Subscrs]} ).
-
-unsubscribe(Ref, Channels)
-    #channel{subscribers=Subscrs}=Channel = lists:keyfind(Chname, 2, Channels),
-    lists:keyreplace(
-        Chname, 2, Channels,
-        Channel#channel{subscribers=lists:keydelete(Ref, 1, Subscrs)} ).
-
-event(Chname, Event, Channels) ->
-    #channel{subscribers=Subscrs}=Channel = lists:keyfind(Chname, 2, Channels),
-    doevent(Subscrs, Event).
+add_handler(Chname, Item, #evtsys{channels=Channels}=State) ->
+    case lists:keyfind(Chname, 2, Channels) of
+        false ->
+            {false, State};      % TODO : Log error ?
+        #channel{subscribers=Subs}=Channel ->
+            Chnl = Channel#channel{subscribers=[Item | Subs]},
+            {true,
+             State#evtsys{channels=lists:keyreplace(Chname, 2, Channels, Chnl)}
+            }
+    end.
 
 
-doevent([], Event) -> ok;
+remove_handler(Chname, Ref, #evtsys{channels=Channels}=State) ->
+    case lists:keyfind(Chname, 2, Channels) of
+        false ->
+            State;              % TODO : Log error ?
+        #channel{subscribers=Subs}=Channel ->
+            Chnl = Channel#channel{subscribers=lists:keydelete(Ref, 2, Subs)},
+            State#evtsys{channels=lists:keyreplace(Chname, 2, Channels, Chnl)}
+    end.
+
+
+doevent([], _) -> ok;
 doevent([{Ref, M, F, Args} | Rest], Event) ->
-    erlang:apply(M, F, [Event | Args]),
-    doevent(Rest, Event).
+    erlang:spawn_link(M, F, [Ref, Event | Args]),
+    doevent(Rest, Event);
 doevent([{Ref, Proc} | Rest], Event) ->
-    Proc ! Event,
+    Proc ! {Ref, Event},
     doevent(Rest, Event).
 
 
-nodepath(Name, #node{content=Content, name=Name}=Node) ->
-    [Node];
-nodepath(Name, #node{content=Content}=Node) ->
-    case nodepath(Node#node.content) of
-        [] -> [];
-        Ls -> [ Node | Ls ];
+bubble_event(_, []) -> ok;
+bubble_event(Event, [#node{name=Name}, Nodes]) ->
+    list_to_atom(Name) ! Event,
+    bubble_event(Event, Nodes).
+
+nodepath(_, []) -> [];
+nodepath(Name, [#text{} | CNodes]) -> nodepath(Name, CNodes);
+nodepath(Name, [#node{}=CNode | CNodes]) -> 
+    case nodepath(Name, CNode) of
+        [] -> nodepath(Name, CNodes);
+        Ls -> Ls
     end;
-nodepath(Name, []) ->
-    [];
-nodepath(Name, [#text{} | CNodes]) ->
-    nodepath(CNodes);
-nodepath(Name, [#node{}=CNode | CNodes]) ->
-    nodepath(CNode)
-
-
-bubbleup([], Event) -> ok;
-bubbleup([Node | Nodes], Event) ->
-    Name = list_to_atom(Node#node.name),
-    Name ! Event,
-    bubbleup(Nodes, Event).
-
-
-
+nodepath(Name, #node{name=Name}=Node) -> [Node];
+nodepath(Name, #node{content=Content}=Node) ->
+    case nodepath(Name, Content) of
+        [] -> [];
+        Ls -> [Node | Ls]
+    end.
--- ncurses/src/ncnode.erl
+++ ncurses/src/ncnode.erl
@@ -1,78 +1,49 @@
 -module(ncnode).
 -author('prataprc@gmail.com').
--behaviour(gen_server).
 
 % module API
--export([start/1, shutdown/1]).
+-export([start_link/1]).
 
-% behaviour Callbacks
--export([
-     init/1, terminate/2, handle_call/3, handle_cast/2, handle_info/2,
-	 code_change/3
-]).
+-export([node_handler/1]).
 
 -include_lib("ncurses.hrl").
 -include_lib("ncdom.hrl").
 
 %---- module API
-start({PPid, Node}) -> ?MODULE:start({PPid, Node, Node});
-start({PPid, RootNode, Node}) ->
-    Name = list_to_atom( Node#node.name ),
-    gen_server:start({local, Name}, ?MODULE, {PPid, RootNode, Node}, []).
-
-
-shutdown(Node) ->
-    Name = list_to_atom( Node#node.name ),
-    gen_server:call(Name, shutdown, infinity).
-
-%---- behaviour Callbacks
-init({ParentPid, RootNode, Node}) ->
-    process_flag(trap_exit, true),
-    error_logger:info_msg("ncnode ~p : ~p~n", [Node#node.name, self()]),
-    Tup = {self(), RootNode},
-    ChildPids = spawn_childnodes(Tup, Node#node.content, []),
-    Box = Node#node.box,
-    {ok, #nodew{
-            pproc=ParentPid,
-            cprocs=ChildPids,
-            pid=self(),
-            rootnode=RootNode,
-            node=Node,
-            box=Box,
-            view=Box#box.view}}.
-
-
-% To be received only for the root node.
-handle_call(shutdown, _From, State) ->
-    % TODO : Handle DOM shutdown, by something like `unload`.
-    {reply, ok, State}.
-
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-
-handle_cast(_, State) ->
-    {noreply, State}.
-
-
-code_change(_, State, _) ->
-    {noreply, State}.
-
-
-terminate(normal, Name) ->
-    error_logger:info_msg("ncnode ~p terminating : ~p~n", [Name, normal]);
-
-terminate(Reason, State) ->
-    Name = State#nodew.node#node.name,
-    error_logger:info_msg("ncnode ~p terminating : ~p~n", [Name, Reason]).
+start_link({PPid, Node}) -> ?MODULE:start_link({PPid, Node, Node});
+start_link({_, _, Node}=Args) ->
+    Pid = erlang:spawn_link(?MODULE, node_handler, Args),
+    erlang:register( list_to_atom(Node#node.name), Pid ),
+    {ok, Pid}.
 
 %---- internal functions
-spawn_childnodes(_, [], Acc) -> lists:reverse( Acc );
-spawn_childnodes(Tup, [#text{} | CNodes], Acc) ->
-    spawn_childnodes(Tup, CNodes, Acc);
-spawn_childnodes(Tup, [#node{}=CNode | CNodes], Acc) ->
-    {ParentPid, RootNode} = Tup,
-    {ok, CPid} = ?MODULE:start({ParentPid, RootNode, CNode}),
-    spawn_childnodes(Tup, CNodes, [CPid | Acc]).
+node_handler({ParentPid, RootNode, Node}) ->
+    process_flag(trap_exit, true),
+    Tup = {self(), RootNode},
+    ChildPids = spawn_kids(Tup, Node#node.content, []),
+    Box = Node#node.box,
+    error_logger:info_msg("ncnode ~p : ~p~n", [Node#node.name, self()]),
+    State = #nodew{pproc=ParentPid,
+                   cprocs=ChildPids,
+                   pid=self(),
+                   rootnode=RootNode,
+                   node=Node,
+                   box=Box,
+                   view=Box#box.view},
+    msgloop(State).
+    
+msgloop(State) ->
+    NewState = 
+        receive
+            {_From, shutdown} -> % To be received only for the root node.
+                % TODO : Handle DOM shutdown, by something like `unload`.
+                State
+        end,
+    msgloop(NewState).
 
+spawn_kids(_, [], Acc) -> lists:reverse( Acc );
+spawn_kids(Tup, [#text{} | CNodes], Acc) -> spawn_kids(Tup, CNodes, Acc);
+spawn_kids(Tup, [#node{}=CNode | CNodes], Acc) ->
+    {ParentPid, RootNode} = Tup,
+    {ok, CPid} = ?MODULE:start_link({ParentPid, RootNode, CNode}),
+    spawn_kids(Tup, CNodes, [CPid | Acc]).
